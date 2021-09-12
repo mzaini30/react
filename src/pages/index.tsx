d36599c59335ec9ed7384918f4571b1c54f09bae
mzaini30 <muhzaini30@gmail.com>
2021-09-12 15:44:03 +0800

    oke

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,12 @@ import {Link} from 'react-router-dom'
 import './index.pcss'
 
 export default function(){
+	import("./about")
+
 	document.title = 'Beranda'
 
 	return <div id='index'>
 		<h1 className='teksBesar'>Beranda</h1>
-		<p>Menuju ke <Link to='/about' onMouseEnter={() => import('./about')}>about</Link></p>
+		<p>Menuju ke <Link to='/about'>about</Link></p>
 	</div>
 }
