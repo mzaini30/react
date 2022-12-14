5756fbb621e51ba4565139e50697c38c6f0dcacd
Zen <muhzaini30@gmail.com>
2022-12-14 08:40:53 +0800

    Oke

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,19 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import WindiCSS from "vite-plugin-windicss";
 import Pages from "vite-plugin-pages";
+import legacy from "@vitejs/plugin-legacy";
+
+let plugins = [react(), WindiCSS(), Pages()];
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), WindiCSS(), Pages()],
+export default defineConfig(({ mode }) => {
+  if (mode == "android") {
+    return {
+      plugins: [legacy(), ...plugins],
+    };
+  } else {
+    return {
+      plugins: [...plugins],
+    };
+  }
 });
